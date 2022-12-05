db7d7eb4fb75c249a9c111ff20774a11a9e61e7c
bolexs <boluwatufeadeojo@gmail.com>
2022-12-05 16:01:54 +0100

    more requirements for project

--- auth_wiki/authwiki/models.py
+++ auth_wiki/authwiki/models.py
@@ -8,3 +8,6 @@ class Users(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=100)
     
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
