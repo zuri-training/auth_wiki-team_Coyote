5c5729331cf76ec4b33a056ce303e85929a48823
Jaiyeola Opeoluwa <geniusjoet1209@gmail.com>
2022-12-17 12:15:41 +0100

    m

--- auth_wiki/auth_wiki/settings.py
+++ auth_wiki/auth_wiki/settings.py
@@ -11,10 +11,11 @@ from pathlib import Path
 import os
 from decouple import config
 from datetime import  timedelta
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
